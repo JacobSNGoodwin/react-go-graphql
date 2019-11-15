e59361be95420143369e9b1ac4148028f3c583d6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-11-14 16:01:42 -0800

    Add a Product Type

--- server/gql/types.go
+++ server/gql/types.go
@@ -1,1 +1,18 @@
 package gql
+
+import "github.com/graphql-go/graphql"
+
+// Product holds graphql type for a product... duh
+var Product = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Product",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.Int,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
