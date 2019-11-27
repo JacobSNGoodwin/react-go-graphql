f0b3b6352fec38b168b9d6a6a350a8847cca163f
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-11-27 14:37:46 -0800

    Adjust port to type string

--- server/database/database.go
+++ server/database/database.go
@@ -14,7 +14,7 @@ import (
 // Database holds connection string info for that database
 type Database struct {
 	Host    string
-	Port    int
+	Port    string
 	User    string
 	Name    string
 	SSLMode string
@@ -25,7 +25,7 @@ var ctxLogger = logger.CtxLogger
 
 // Connect returns the DB connection for the connection info in the struct
 func (d *Database) Connect() {
-	connStr := "host=%s port=%d user=%s dbname=%s sslmode=%s"
+	connStr := "host=%s port=%s user=%s dbname=%s sslmode=%s"
 	connStr = fmt.Sprintf(connStr, d.Host, d.Port, d.User, d.Name, d.SSLMode)
 
 	db, err := gorm.Open("postgres", "host=localhost port=5432 user=user dbname=gql_demo password=password sslmode=disable")
