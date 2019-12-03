e8f14ded64dc3813c68e8747974689d142c3053d
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-12-03 12:10:43 -0800

    Get id_token for google login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  GoogleLogin,
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline
+} from "react-google-login";
 
 const App: React.FC = () => {
+  const responseGoogle = (
+    res: GoogleLoginResponse | GoogleLoginResponseOffline
+  ) => {
+    if ((res as GoogleLoginResponse).getAuthResponse) {
+      console.log((res as GoogleLoginResponse).getAuthResponse().id_token);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId="23806398157-4i36lv7vhdh3k3j5gnspggm75qg83o5v.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
-}
+};
 
 export default App;
