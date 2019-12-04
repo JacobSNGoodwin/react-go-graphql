328efea03aa3606f001d03795daf97f7ee8fc4ff
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-12-03 18:27:53 -0800

    Add login with FB to client
    Will deal with styles later

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import {
   GoogleLogin,
   GoogleLoginResponse,
   GoogleLoginResponseOffline
 } from "react-google-login";
+import FacebookLogin, { ReactFacebookLoginInfo } from "react-facebook-login";
 
 const App: React.FC = () => {
   const responseGoogle = (
@@ -15,14 +16,32 @@ const App: React.FC = () => {
     }
   };
 
+  const responseFacebook = (res: ReactFacebookLoginInfo) => {
+    console.log(res);
+  };
+
+  const googleClientid: string = process.env.REACT_APP_GOOGLE_CLIENT_ID
+    ? process.env.REACT_APP_GOOGLE_CLIENT_ID
+    : "";
+  const fbClientid: string = process.env.REACT_APP_FACEBOOK_CLIENT_ID
+    ? process.env.REACT_APP_FACEBOOK_CLIENT_ID
+    : "";
+
   return (
     <div className="App">
       <GoogleLogin
-        clientId="23806398157-4i36lv7vhdh3k3j5gnspggm75qg83o5v.apps.googleusercontent.com"
+        clientId={googleClientid}
         buttonText="Login"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
         cookiePolicy={"single_host_origin"}
+      />
+      <FacebookLogin
+        appId={fbClientid}
+        textButton="Login"
+        fields="name,email,picture"
+        callback={responseFacebook}
+        icon="fa-facebook"
       />
     </div>
   );
