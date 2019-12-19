32ec90c88584db163772f1803d2b3b16a4611a65
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-12-18 16:40:42 -0800

    Add link to users and login

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,8 +2,26 @@ import React from "react";
 import { Link } from "@reach/router";
 
 const navbar: React.FC = props => (
-  <nav className="navbar is-dark">
+  <nav className="navbar is-fixed-top">
+    <div className="navbar-brand">
+      <Link to="/" className="navbar-item">
+        <img
+          src={`${process.env.PUBLIC_URL}/android-chrome-192x192.png`}
+          alt="Home Link Logo"
+        />
+      </Link>
+      <button className="navbar-burger burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
     <div className="navbar-menu">
+      <div className="navbar-start">
+        <Link to="/users" className="navbar-item">
+          Users
+        </Link>
+      </div>
       <div className="navbar-end">
         <div className="navbar-item">
           <Link to="/login" className="button is-link">
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "bulma/bulma.sass";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
