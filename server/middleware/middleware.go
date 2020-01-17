e523b1b6ddefcf56e3396ce4412f2626f72ba0da
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-01-16 19:58:56 -0800

    Revert X-request-header check
    Blocks use of playground

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -2,13 +2,10 @@ package middleware
 
 import (
 	"context"
-	"encoding/json"
 	"net/http"
 	"os"
 
 	"github.com/go-redis/redis"
-	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/graphql/gqlerrors"
 	"github.com/graphql-go/handler"
 	"github.com/maxbrain0/react-go-graphql/server/logger"
 	"github.com/maxbrain0/react-go-graphql/server/models"
@@ -26,29 +23,29 @@ type Config struct {
 func HTTPMiddleware(c *Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Check for X-Requested-With header
-		h := r.Header.Get("X-Requested-With")
+		// h := r.Header.Get("X-Requested-With")
 
-		if h != "XMLHttpRequest" {
-			e := gqlerrors.NewFormattedError("Bad Request. Must include X-Requested-With header")
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(graphql.Result{
-				Errors: []gqlerrors.FormattedError{e},
-			})
-		} else {
-			// Get cookies and reconstruct token - verify token and append authorization roles to
-			// the req context
-			ctxUser := userFromCookies(&w, r, c.R)
+		// if h != "XMLHttpRequest" {
+		// 	e := gqlerrors.NewFormattedError("Bad Request. Must include X-Requested-With header")
+		// 	w.Header().Set("Content-Type", "application/json")
+		// 	w.WriteHeader(http.StatusOK)
+		// 	json.NewEncoder(w).Encode(graphql.Result{
+		// 		Errors: []gqlerrors.FormattedError{e},
+		// 	})
+		// }
+		// Get cookies and reconstruct token - verify token and append authorization roles to
+		// the req context
+		ctxUser := userFromCookies(&w, r, c.R)
 
-			ctx := context.WithValue(r.Context(), models.ContextKeyUser, ctxUser)
-			ctx = context.WithValue(ctx, models.ContextKeyWriter, &w)
+		ctx := context.WithValue(r.Context(), models.ContextKeyUser, ctxUser)
+		ctx = context.WithValue(ctx, models.ContextKeyWriter, &w)
 
-			w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CLIENT_HOST"))
-			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-Requested-With")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Origin", os.Getenv("CLIENT_HOST"))
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-Requested-With")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-			c.GQLHandler.ContextHandler(ctx, w, r)
-		}
+		c.GQLHandler.ContextHandler(ctx, w, r)
+
 	})
 }
