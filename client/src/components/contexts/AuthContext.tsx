4ca3e31483c68ec746a1af22c1551097eede6431
Jacob Goodwin <jacob.goodwin@gmail.com>
2020-01-20 20:21:44 -0800

    Fix annoying front end logout bug (even though I shouldn't)

--- client/src/components/contexts/AuthContext.tsx
+++ client/src/components/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useMutation, useQuery } from "@apollo/react-hooks";
-import { navigate } from "@reach/router";
+import { navigate, redirectTo } from "@reach/router";
 import Cookies from "js-cookie";
 
 import { LOGIN_GOOGLE, LOGIN_FACEBOOK } from "../../gql/mutations";
@@ -132,9 +132,8 @@ const AuthProvider: React.FC = props => {
 
   const logout = () => {
     Cookies.remove("userinfo");
-    navigate("/login").then(_ => {
-      setUser(undefined);
-    });
+    setUser(undefined);
+    redirectTo("/login");
   };
 
   if (googleLoading || facebookLoading || meLoading) {
